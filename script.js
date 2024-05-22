2b8bcd160d84d60bd0883e4261c7a0b6f2dce19d
Mari Ukne <mariukne@gmail.com>
2024-05-22 11:16:02 +0000

    Fixed a bug in showResult function in JavaScript

--- script.js
+++ script.js
@@ -1,92 +1,201 @@
-let questions = [
-    {
+let questions = [{
         question: 'Which dog breed is the largest?',
-        answers: [
-            { text: 'Chihuahua', correct: false },
-            { text: 'Siberian Husky', correct: false },
-            { text: 'Great Dane', correct: true },
-            { text: 'Poodle', correct: false },
+        answers: [{
+                text: 'Chihuahua',
+                correct: false
+            },
+            {
+                text: 'Siberian Husky',
+                correct: false
+            },
+            {
+                text: 'Great Dane',
+                correct: true
+            },
+            {
+                text: 'Poodle',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which dog breed is the smallest?',
-        answers: [
-            { text: 'Chihuahua', correct: true },
-            { text: 'Pomeranian', correct: false },
-            { text: 'Shih Tzu', correct: false },
-            { text: 'Pug', correct: false },
+        answers: [{
+                text: 'Chihuahua',
+                correct: true
+            },
+            {
+                text: 'Pomeranian',
+                correct: false
+            },
+            {
+                text: 'Shih Tzu',
+                correct: false
+            },
+            {
+                text: 'Pug',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which country does the dog breed Beagle come from?',
-        answers: [
-            { text: 'United Kingdom', correct: true },
-            { text: 'France', correct: false },
-            { text: 'Italy', correct: false },
-            { text: 'Spain', correct: false },
+        answers: [{
+                text: 'United Kingdom',
+                correct: true
+            },
+            {
+                text: 'France',
+                correct: false
+            },
+            {
+                text: 'Italy',
+                correct: false
+            },
+            {
+                text: 'Spain',
+                correct: false
+            },
         ]
     },
-    { 
+    {
         question: 'Which country does Bernese Mountain Dog come from?',
-        answers: [
-            { text: 'Sweden', correct: false },
-            { text: 'Russia', correct: false },
-            { text: 'Ireland', correct: false },
-            { text: 'Schweiz', correct: true },
+        answers: [{
+                text: 'Sweden',
+                correct: false
+            },
+            {
+                text: 'Russia',
+                correct: false
+            },
+            {
+                text: 'Ireland',
+                correct: false
+            },
+            {
+                text: 'Schweiz',
+                correct: true
+            },
         ]
     },
     {
         question: 'Which dog breed is most commonly used in dog race?',
-        answers: [
-            { text: 'Golden Retriever', correct: false},
-            { text: 'Greyhound', correct: true },
-            { text: 'Japanese Spitz', correct: false },
-            { text: 'Poodle', correct: false },
+        answers: [{
+                text: 'Golden Retriever',
+                correct: false
+            },
+            {
+                text: 'Greyhound',
+                correct: true
+            },
+            {
+                text: 'Japanese Spitz',
+                correct: false
+            },
+            {
+                text: 'Poodle',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which dog breed is the heaviest?',
-        answers: [
-            { text: 'Chihuahua', correct: false },
-            { text: 'English Mastiff', correct: true },
-            { text: 'Saint Bernard', correct: false},
-            { text: 'Newfoundland', correct: false },
+        answers: [{
+                text: 'Chihuahua',
+                correct: false
+            },
+            {
+                text: 'English Mastiff',
+                correct: true
+            },
+            {
+                text: 'Saint Bernard',
+                correct: false
+            },
+            {
+                text: 'Newfoundland',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which dog breed is one of the oldest?',
-        answers: [
-            { text: 'Golden Retriever', correct: false },
-            { text: 'Bullddog', correct: false },
-            { text: 'Greyhound', correct: true},
-            { text: 'Poodle', correct: false },
+        answers: [{
+                text: 'Golden Retriever',
+                correct: false
+            },
+            {
+                text: 'Bullddog',
+                correct: false
+            },
+            {
+                text: 'Greyhound',
+                correct: true
+            },
+            {
+                text: 'Poodle',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which dog breed is known for being used as a police dog due to its intelligence and trainabillity?',
-        answers: [
-            { text: 'German Shepherd', correct: true },
-            { text: 'Pug', correct: false },
-            { text: 'Labrador Retriever', correct: false },
-            { text: 'Dalmatian', correct: false },
+        answers: [{
+                text: 'German Shepherd',
+                correct: true
+            },
+            {
+                text: 'Pug',
+                correct: false
+            },
+            {
+                text: 'Labrador Retriever',
+                correct: false
+            },
+            {
+                text: 'Dalmatian',
+                correct: false
+            },
         ]
     },
     {
         question: 'Which dog breed is known for its distinctive blue-black tongue?',
-        answers: [
-            { text: 'Bulldog', correct: false },
-            { text: 'Chow Chow', correct: true },
-            { text: 'Chihuahua', correct: false },
-            { text: 'Pomeranian', correct: false },
+        answers: [{
+                text: 'Bulldog',
+                correct: false
+            },
+            {
+                text: 'Chow Chow',
+                correct: true
+            },
+            {
+                text: 'Chihuahua',
+                correct: false
+            },
+            {
+                text: 'Pomeranian',
+                correct: false
+            },
         ]
     },
     {
         question: 'How many dog breeds are recognized worldwide by the Fédération Cynologique Internationale (FCI)?',
-        answers: [
-            { text: '200', correct: false },
-            { text: '475', correct: false },
-            { text: '360', correct: true },
-            { text: '150', correct: false },
+        answers: [{
+                text: '200',
+                correct: false
+            },
+            {
+                text: '475',
+                correct: false
+            },
+            {
+                text: '360',
+                correct: true
+            },
+            {
+                text: '150',
+                correct: false
+            },
         ]
     },
 ];
@@ -98,15 +207,15 @@ let timer;
 let timeLeft = 15;
 
 let questionElement =
-document.getElementById('question');
+    document.getElementById('question');
 let answerButtonsElement =
-document.getElementById('answer-buttons');
+    document.getElementById('answer-buttons');
 let correctElement =
-document.getElementById('correct');
+    document.getElementById('correct');
 let incorrectElement =
-document.getElementById('incorrect');
-let timerElement = 
-document.getElementById('timer');
+    document.getElementById('incorrect');
+let timerElement =
+    document.getElementById('timer');
 
 function startQuiz() {
     showQuestion(questions[currentQuestionIndex]);
@@ -118,32 +227,32 @@ function showQuestion(question) {
 
     question.answers.forEach(answer => {
         let button =
-        document.createElement('button');
+            document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('btn');
         button.addEventListener('click', () => {
             if (answer.correct) {
                 correctCount++;
                 correctElement.innerText = correctCount;
-                
+
             } else {
                 incorrectCount++;
                 incorrectElement.innerText =
-                incorrectCount;
+                    incorrectCount;
             }
 
             if (currentQuestionIndex < questions.length - 1) {
                 currentQuestionIndex++;
 
-            showQuestion(questions[currentQuestionIndex]);
-            
-        } else {
-            clearInterval(timer);
-            showResult();
-        }
-     });
+                showQuestion(questions[currentQuestionIndex]);
 
-     answerButtonsElement.appendChild(button);
+            } else {
+                clearInterval(timer);
+                showResult();
+            }
+        });
+
+        answerButtonsElement.appendChild(button);
     });
     resetTimer();
 }
@@ -158,13 +267,12 @@ function startTimer() {
             incorrectCount++;
             incorrectElement.innerText = incorrectCount;
             currentQuestionIndex++;
-            if (currentQuestionIndex < questions.length)
-                {
-                    showQuestion(questions[currentQuestionIndex]);
-                } else {
-                    showResult();
-                }
-         }
+            if (currentQuestionIndex < questions.length) {
+                showQuestion(questions[currentQuestionIndex]);
+            } else {
+                showResult();
+            }
+        }
     }, 1000);
 }
 
@@ -176,9 +284,10 @@ function resetTimer() {
 
 function showResult() {
     questionElement.innerText = 'Quiz completed! Your result:';
-    answerButtonsElement.innerHTML =
-    '<p>Correct answers: ${correctCount}</p>'
-    '<p>Incorrect answers: ${incorrectCount}</p>';
+    answerButtonsElement.innerHTML = `
+    <p>Correct answers: ${correctCount}</p>
+    <p>Incorrect answers: ${incorrectCount}</p>
+  `;
 }
 
 startQuiz();
